7eb076fe96a76bde785d43b34b0a040981a74fea
Jared Goodwin <jared.goodwin@wsu.edu>
2017-12-03 20:31:31 -0800

    Search results table now populates based on database values

--- static/scripts/talink.js
+++ static/scripts/talink.js
@@ -712,11 +712,14 @@ var TALink = (function(){
 	
     var attachStudentCourseSearchListener = function(e){
         var onSuccess = function(data){
-
+		$("#course-search-results-table-body").html("");
+		
 			for (var i = 0; i < data["found_courses"].length; i++){
 				alert(data["found_courses"][i].course_name + "\n" +
 					data["found_courses"][i].days_lecture + "\n" +
 					"\n");
+					
+				makeCourseSearchTableEntry(data["found_courses"][i]);
 
 				//console.log(data["found_courses"][i].course_name);
 			}
@@ -734,8 +737,21 @@ var TALink = (function(){
         });
     }
 	
+	var makeCourseSearchTableEntry = function(course){
+		var courseNameSplit = course.course_name.split(" ");
+		
+		$("#course-search-results-table-body").append($('<tr/>')
+		.append($('<th/>').html("&#x2610"),
+			($('<th/>').attr('scope', 'row').text(courseNameSplit[0])),
+			($('<td/>').text(courseNameSplit[1])),
+			($('<td/>').text(course.semester)),
+			($('<td/>').text(course.days_lecture)),
+			($('<td/>').text(course.time_lecture))
+		)
+	)}
+	
     
-      var attachInstructorCourseApplicantListener = function(e){
+    var attachInstructorCourseApplicantListener = function(e){
         
         //window.alert(document.getElementById("selected-prefix")).innerHTML;
         $(".class-list2").on("click", ".instructor-class-info", function(e){
